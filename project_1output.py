b40fa44c3f0308e5a5910da89933ee845493ef26
chaitapatel <chaitapatel2789@gmail.com>
2026-08-27 11:44:43 +0530

    Add files via upload

---
+++ project_1output.py
@@ -0,0 +1,17 @@
+#Welcome to the Interactive Personal Data Collector!
+#Please enter your name: chaita
+#Please enter your age: 17
+#Please enter your height in meters: 5.4
+#Please enter your favourite number: 38
+
+#Thank you! Here is the information we collected:
+
+#Name: chaita (Type: <class 'str'>, Memory Address: 1713171023888)
+#Age: 17 (Type: <class 'int'>, Memory Address: 140718914573736)
+#Height: 5.4 (Type: <class 'float'>, Memory Address: 1713170782768)
+#Favourite Number: 38 (Type: <class 'int'>, Memory Address: 140718914574408)
+
+#Your birth year is approximately: 2009 (based on your age of 17)
+
+#Thank you for using the Personal Data Collector.
+#Goodbye! Keep exploring Python!
